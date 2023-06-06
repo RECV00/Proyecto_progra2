ad9374ad6c7a16236113185cd65a3ef66badac6f
KeynaCaFu <keynacastrofuentes@gmail.com>
2023-06-05 18:21:53 -0600

    xml

--- src/data/FilesXML.java
+++ src/data/FilesXML.java
@@ -1,19 +1,115 @@
 package data;
 
 import java.io.File;
+import java.io.IOException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 public class FilesXML {
 
+public void createXML(String objectName,String fileName) {
+
+		File file = new File(fileName);
+
+		if(!file.exists()) {
+			try {
+				DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+				DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+
+				Document doc = dBuilder.newDocument();
+
+				Element rootElement = doc.createElement(objectName);
+				doc.appendChild(rootElement);
+
+				TransformerFactory transformerFactory = TransformerFactory.newInstance();
+				Transformer transformer = transformerFactory.newTransformer();
+
+				DOMSource source = new DOMSource(doc);
+
+				StreamResult result = new StreamResult(file);
+				transformer.transform(source, result);
+
+				System.out.println("Archivo Creado");
+
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+public void writeXML(String FileName, String elementType, String[] dataName, String[] data) {
+
+		try {
+
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+			DocumentBuilder db = dbf.newDocumentBuilder();
+
+			Document doc = db.parse(new File(FileName));
+			doc.getDocumentElement().normalize();
+
+			Element rootElement  =  doc.getDocumentElement();
+
+			Element ele = doc.createElement(elementType);
+			rootElement.appendChild(ele);
+//la informacion dentro del XML
+			Attr attr = doc.createAttribute(dataName[0]);//pestalla para ver datos 
+			attr.setValue(data[0]);
+			ele.setAttributeNode(attr);
+
+			for(int i = 1; i < data.length; i++){//cuenta desde el atributo siguiente 
+
+				Element dato = doc.createElement(dataName[i]);
+
+				dato.appendChild(doc.createTextNode(data[i]));//saca los datos
+
+				ele.appendChild(dato);//agrega
+			}
+
+			//escribirmos el contenido en un archivo xml
+			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+			Transformer transformer = transformerFactory.newTransformer();
+
+			DOMSource source = new DOMSource(doc);
+
+			StreamResult result = new StreamResult(new File(FileName));
+			transformer.transform(source, result);
+
+			System.out.println("Registro Guardado");
+
+		}catch(ParserConfigurationException pce) {
+
+			pce.printStackTrace();
+
+		}catch (SAXException e) {
+
+			e.printStackTrace();
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		} catch (TransformerConfigurationException e) {
+
+			e.printStackTrace();
+		} catch (TransformerException e) {
+
+			e.printStackTrace();
+		}
+}
+	
 	public static void deleteLine(String archive, String wordDelete) {
 	    try {
 	        // Leer el archivo XML y cargarlo en un objeto Document
@@ -46,4 +142,41 @@ public class FilesXML {
 	    }
 	}
 
+public String validateUser(String username, String password) throws Exception {
+	    // Load the XML file
+	    File file = new File("users.xml");
+	    DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+	    DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+	    Document doc = dBuilder.parse(file);
+	    doc.getDocumentElement().normalize();
+
+	    // Get the list of user nodes
+	    NodeList userList = doc.getElementsByTagName("user");
+
+	    // Loop through each user
+	    for (int i = 0; i < userList.getLength(); i++) {
+	        Node userNode = userList.item(i);
+	        if (userNode.getNodeType() == Node.ELEMENT_NODE) {
+	            Element user = (Element) userNode;
+
+	            // Get the user's credentials and status
+	            String userUsername = user.getElementsByTagName("username").item(0).getTextContent();
+	            String userPassword = user.getElementsByTagName("password").item(0).getTextContent();
+	            String userStatus = user.getElementsByTagName("status").item(0).getTextContent();
+
+	            // Check if the username and password match
+	            if (username.equals(userUsername) && password.equals(userPassword)) {
+	                // Check if the user is active
+	                if (userStatus.equals("active")) {
+	                    // Return the user's type
+	                    return user.getElementsByTagName("type").item(0).getTextContent();
+	                } else {
+	                    throw new Exception("User is not active");
+	                }
+	            }
+	        }
+	    }
+
+	    throw new Exception("User not found");
+	}
 }
