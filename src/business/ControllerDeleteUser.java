f1920301671a81a72738d7521fc106d9be55a5f5
RECV00 <corralesreychell@gmail.com>
2023-06-20 11:45:50 -0600

    gf

--- src/business/ControllerDeleteUser.java
+++ src/business/ControllerDeleteUser.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.Vector;
 
 import data.FilesXML;
+import data.Logic;
 import domain.User;
 import presentation.GUIDeleteUser;
 
@@ -13,11 +14,16 @@ public class ControllerDeleteUser implements ActionListener{
 	private GUIDeleteUser guiDU;
 	private FilesXML fXML;
 	private User user;
+	private Logic lo;
+	private ArrayList<User>arrayLUser;
+	
 	
 	public ControllerDeleteUser() {
 		// TODO Auto-generated constructor stub
 		guiDU= new GUIDeleteUser();
 		fXML= new FilesXML();
+		lo= new Logic();
+		user = new User();
 		initializer();
 	}
 
@@ -30,15 +36,13 @@ private void initializer() {
 	@Override
 public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		
+		guiDU.getDTMTUser().setRowCount(0);
+		guiDU.setArrayListUser(lo.getListUser("Users.xml","User"));
+		guiDU.fillTable(guiDU.getArrayListUser());
 		
 		if(e.getSource()== guiDU.getBDelete()) {
-			//Object data = fXML.readXML("Users.xml");
 			
-			guiDU.getDTMTUser().setRowCount(0);
-			//guiDU.getArrayListUser().remove(0);
-			guiDU.fillTable(guiDU.getArrayListUser());
-			
+		fXML.deleteXML("Users.xml",guiDU.getTNameDelete().getText());
 			
 		}
 		if(e.getSource()== guiDU.getBExit()) {
